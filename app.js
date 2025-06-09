e17c2d523264f7223b52ff5a49d874689970da97
ezequiel <ezequiel.calomino@gmail.com>
2025-06-09 12:48:46 -0600

    primer comit

---
+++ app.js
@@ -0,0 +1,9 @@
+//ejercicio 1
+// crear un array de frutas y agregar a lo ultimo uva
+
+const frutas = ["naranja", "platno", "naranja"];
+console.log("el array original es " + frutas);
+frutas.push("uva");
+console.log("el array modificado es " + frutas);
+frutas.pop(); //borra el ultimo elemento
+//ejercicio 2
