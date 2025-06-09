939ba3b1781700f95b96b38454eb8ad9ce4282d0
ezequiel <ezequiel.calomino@gmail.com>
2025-06-09 13:05:43 -0600

    ejercicio 2

--- app.js
+++ app.js
@@ -7,3 +7,10 @@ frutas.push("uva");
 console.log("el array modificado es " + frutas);
 frutas.pop(); //borra el ultimo elemento
 //ejercicio 2
+const numeros =[1, 2, 3, 4, 5];
+console.log("array original: " + numeros);
+
+const duplicados = numeros.map((num) =>{
+    return num*2;
+});
+console.log("array duplicado: " + duplicados);
