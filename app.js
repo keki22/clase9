20783e06b254aea178d2a8ce6b16f14ca87e5f6e
ezequiel <ezequiel.calomino@gmail.com>
2025-06-09 13:41:25 -0600

    pasos para subir

--- app.js
+++ app.js
@@ -14,3 +14,18 @@ const duplicados = numeros.map((num) =>{
     return num*2;
 });
 console.log("array duplicado: " + duplicados);
+
+//ejercicio 3
+
+// para crear el repo
+// 1ro git init
+// 2do git add .
+// 3ro git commit -m "nombre del commir"
+// 4to git remote add origin <url del repo>
+// 5to git branch -m main
+// 6to git push -u origin main
+
+// una vez creado el repo
+// 1ro git add .
+// 2do git commit -m "mensaje"
+// 3ro git push
